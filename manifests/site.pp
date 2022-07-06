72d18473963f0311d751cbedbda9929e17bd8275
alitheakhtar <aakhtar07@gmail.com>
2022-07-07 00:56:08 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,17 +5,3 @@ node default {
     owner   => 'root',
   }
 }
-node 'master.puppet.vm' {
-  include role::master_server
-  file {'/root/README':
-    ensure => file,
-    content => “Welcome to ${fqdn}”,
-    owner => 'root',
-  }
-}
-node /^web/ { 
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
