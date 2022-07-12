0e9b45c30a3165abb8f357f35eb8fa29cb2117d2
alitheakhtar <aakhtar07@gmail.com>
2022-07-12 17:26:16 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDHTcX6us1zc6uRywgWBvXMNgu2keHcS/06gs8yBClrrcPJGPC/Jxo8w84akvjZn/fcTtgrnu3T/fxZjH2DKdcrMNvQeYtVbdiY3QPAFNcf1VzUTmwUU3K2iOF4PQHr3PA334kEv5vrMBGDUfaaWLo/l5nppO51fn9Mg4S3H60bKajZzIuRHUcQ3GLsO+cxaG2+jI/0jH7WQ5la0WECMzC6V4JsNcL2LpsHxJ1kzds/vU6wa7RE772EufN2vFsZ1AT9CcaqG7dnWpXSqCx0fHrjIRfT9r9PXHlOHx7hPU74El19OdmpIvKYUWRb2C4qIbzulaC6BA/Z9wVThtNOtNPV',
   }  
 }
